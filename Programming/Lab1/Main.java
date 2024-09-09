b5523831fe29245e4d4cf9d89fa7d342178c3f93
roflex <Stodvalista@yandex.ru>
2024-09-09 14:51:32 +0300

    Del comment

--- Programming/Lab1/Main.java
+++ Programming/Lab1/Main.java
@@ -1,7 +1,5 @@
 import java.util.Random;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     static Random random = new Random();
 
