2c0c2d787a0b8b92020df2c7702eece89f3addc5
roflex <Stodvalista@yandex.ru>
2024-09-16 11:25:51 +0300

    1 часть

---
+++ OPD/Lab1/lab.sh
@@ -0,0 +1,60 @@
+# ====== Создание файлов ======
+
+mkdir lab0
+
+cd lab0
+touch blastoise4
+touch cinccino6
+touch excadrill7
+
+mkdir lillipup2
+cd lillipup2
+touch lileep
+touch armaldo
+touch patrat
+mkdir quilava
+touch trubbish
+mkdir vanilluxe
+cd ..
+
+mkdir mamoswineO
+cd mamoswineO
+mkdir mienfoo
+touch vanillite
+touch glaceon
+touch purrloin
+cd ..
+
+mkdir volcarona5
+cd volcarona5
+touch seismitoad
+mkdir masquerain
+mkdir shellder
+mkdir magikarp
+cd ..
+
+# ====== Заполнение файлов ======
+
+echo $"Тип покемона WATER NONE" > blastoise4
+echo $"Тип покемона NORMAL" > cinccino6
+echo $"Тип диеты Herbivore" > excadrill7
+
+cd lillipup2
+echo $"Тип покемона ROCK\nGRASS" > lileep
+echo $"Тип покемона ROCK BUG" > armaldo
+echo $"Живёт Forest\nGrassland" > patrat
+echo $"Способности Venom Stench Sticky\nHold" > trubbish
+cd ../mamoswineO
+
+echo $"satk=7 sdef=6 spd=5" > vanillite
+echo $"Тип покемона ICE\nNONE" > glaceon
+echo $"Способности Dark Art Limber\nUnburden" > purrloin
+cd ../volcarona5
+
+echo $"Способности Supersonic Round Bubblebeam Mud Shot\nAqua Ring Uproar Muddy Water Rain Dance Acid Flail Drain Punch Echoed\nVoice Hydro Pump Hyper Voice" > seismitoad
+
+
+
+
+
+pause
