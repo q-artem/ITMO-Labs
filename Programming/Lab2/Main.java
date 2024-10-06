2ea493c881e2bdcb0cf3ef64459047fd5e4a503f
RofleX <94610518+RofIeX@users.noreply.github.com>
2024-10-06 22:33:02 +0300

    lab2

---
+++ Programming/Lab2/Main.java
@@ -0,0 +1,22 @@
+import ru.ifmo.se.pokemon.*;
+import pokemons.*;
+import lib.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Battle b = new Battle();
+        Pokemon fp1 = new Igglybuff("Микрочел", 10);
+        Pokemon fp2 = new Jigglypuff("Дефолтчел", 7);
+        Pokemon fp3 = new Wigglytuff("Макрочел", 27);
+        b.addAlly(fp1);
+        b.addAlly(fp2);
+        b.addAlly(fp3);
+        Pokemon sp1 = new MimeJr("Бэбс", 10);
+        Pokemon sp2 = new MrMime("Папич", 27);
+        Pokemon sp3 = new Pheromosa("Неономи", 15);
+        b.addFoe(sp1);
+        b.addFoe(sp2);
+        b.addFoe(sp3);
+        b.go();
+    }
+}
