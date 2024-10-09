cbae572b9c8d1083248250ec0c480116cec4bf95
RofleX <94610518+RofIeX@users.noreply.github.com>
2024-10-09 18:57:11 +0300

    lab2 prog finish

--- Programming/Lab2/attacks/Scald.java
+++ Programming/Lab2/attacks/Scald.java
@@ -2,11 +2,25 @@ package attacks;
 
 import ru.ifmo.se.pokemon.*;
 
+import java.util.Random;
+
+
 public class Scald extends SpecialMove {
     public Scald() {
         super(Type.WATER, 80, 100);
     }
 
+
+    @Override
+    public void applyOppEffects(Pokemon opp) {
+        Random random = new Random();
+        if (random.nextFloat(0, 1) < 30) {
+            if (!(opp.hasType(Type.FIRE) || opp.hasType(Type.WATER))) {
+                opp.setMod(Stat.HP, 0);
+            }
+        }
+        opp.setMod(Stat.SPECIAL_DEFENSE, -2);
+    }
 
 
     @Override
--- Programming/Lab2/attacks/ThunderWave.java
+++ Programming/Lab2/attacks/ThunderWave.java
@@ -1,6 +1,25 @@
 package attacks;
 
-import ru.ifmo.se.pokemon.Move;
+import ru.ifmo.se.pokemon.*;
+import java.util.Random;
 
-public class ThunderWave extends Move {
+public class ThunderWave extends StatusMove {
+    public ThunderWave() {
+        super(Type.ELECTRIC, 0, 90);
+    }
+
+    @Override
+    public void applyOppEffects(Pokemon opp) {
+        if (opp.hasType(Type.ELECTRIC)) {
+            Random random = new Random();
+            if (random.nextFloat(0, 1) < 0.25) {
+                Effect.paralyze(opp);
+            }
+        }
+    }
+
+    @Override
+    public String describe() {
+        return "применяет ThunderWave";
+    }
 }
