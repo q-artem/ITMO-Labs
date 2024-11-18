3272af3ec82c898e8e1608605f8b4f75d1278340
roflex <Stodvalista@yandex.ru>
2024-11-18 10:45:03 +0300

    edit lab2 #prog

--- Programming/Lab2/pokemons/Dewgong.java
+++ Programming/Lab2/pokemons/Dewgong.java
@@ -1,16 +1,15 @@
 package pokemons;
 
 import attacks.*;
-import ru.ifmo.se.pokemon.Pokemon;
 import ru.ifmo.se.pokemon.Type;
 
 
-public class Dewgong extends Pokemon {
+public class Dewgong extends Seel {
     public Dewgong(String name, int lvl) {
         super(name, lvl);
         setStats(90, 70, 80, 70, 95, 70);
         addType(Type.WATER);
         addType(Type.ICE);
-        setMove(new Rest(), new SmartStrike(), new IceShard(), new FrostBreath());
+        addMove(new FrostBreath());
     }
 }
--- Programming/Lab2/pokemons/Eelektross.java
+++ Programming/Lab2/pokemons/Eelektross.java
@@ -1,16 +1,13 @@
 package pokemons;
 
-import attacks.AcidSpray;
-import attacks.ChargeBeam;
 import attacks.Confide;
-import attacks.ThunderWave;
-import ru.ifmo.se.pokemon.Pokemon;
 import ru.ifmo.se.pokemon.Type;
 
-public class Eelektross extends Pokemon {
+public class Eelektross extends Eelektrik {
     public Eelektross(String name, int lvl) {
+        super(name, lvl);
         setStats(85, 115, 80, 105, 80, 50);
         addType(Type.ELECTRIC);
-        setMove(new ThunderWave(), new ChargeBeam(), new AcidSpray(), new Confide());
+        addMove(new Confide());
     }
 }
