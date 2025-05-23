f1c486cced23e99626e63342f6a52844d6f5f55d
RofleX <94610518+RofIeX@users.noreply.github.com>
2025-05-23 15:46:44 +0300

    pass paint!!!

--- OPD/Lab5/dop_paint.asm
+++ OPD/Lab5/dop_paint.asm
@@ -1,6 +1,31 @@
+  ; ;;;;;;;;;;;;;;;; Paint в БЭВМ!!!!!! ;;;;;;;;;;;;;;;;
+  
+  ; ;;;;;;;;;;;;; Вывод данных ;;;;;;;;;;;;;;
+  ; ВУ-6 - бегущая строка - поле для рисования
+  ; КВУ-3 - (индикация курсора), на холсте курсор не мигает потому что БЭВМ костыль
+  ; КВУ-1 - Координаты курсора, старшие 4 бита - Y mod 16, младшие 4 бита - X
+  
+  ; ;;;;;;;;;;;;; Ввод данных ;;;;;;;;;;;;;;;
+  ; ВУ-9 - клавиатура
+  ; ;; Управление курсором
+  ; ;;;; "*" - курсор
+  ; ;;;; "+" - карандаш
+  ; ;;;; "-" - ластик
+  ; ;; Управление передвижением курсора
+  ; ;;;; цифры 1, 2, 3, 4, 6, 7, 8, 9 - перемещение курсора в соотв. направлении относительно цифры 5
+  ; ;;;; 0 - сброс положения курсора в ноль
+  ; ;; "/" - очистка холста
+  ; КВУ-3 - кнопка Готов - остановка программы
+  
+  ; ;;;;;;;;;;;;;; Настройки ;;;;;;;;;;;;;;;;
+  ; Для нормальная работы нужно поставить максимальную скорость клавишей F12 и
+  ; отрыть вкладку "БЭВМ"
+
+  ; ;;;;;;;;;;;; (не)приятного пользования! ;;;;;;;;;;;;
+  
     ORG 0x0
 V0: WORD $DEFAULT, 0x180    ; Вектор прерывания #0
-V1: WORD $DEFAULT, 0x180  ; Вектор прерывания #1
+V1: WORD $INT_TIMER, 0x180  ; Вектор прерывания #1
 V2: WORD $DEFAULT, 0x180    ; Вектор прерывания #2
 V3: WORD $DEFAULT, 0x180    ; Вектор прерывания #3
 V4: WORD $DEFAULT, 0x180    ; Вектор прерывания #4
@@ -9,10 +34,10 @@ V6: WORD $DEFAULT, 0x180    ; Вектор прерывания #6
 V7: WORD $DEFAULT, 0x180    ; Вектор прерывания #7
     
 DEFAULT: IRET  ; Дефолтный вектор
-
+         
                                 ; Настройки
 blink_cursor_period: WORD 0x01  ; Период мигания курсора в 1/10 сек.
-
+                     
                           ; Глобальные переменные
 curr_x:       WORD 0x00   ; Текущая координата X (0..7)
 curr_y:       WORD 0x00   ; Текущая координата Y (0..31)
@@ -20,61 +45,74 @@ max_x:        WORD 0x07   ; Максимальный X
 max_y:        WORD 0x1F   ; Максимальный Y и размер массива соотв.
 cursor_state: WORD 0x00   ; Состояние курсора сейчас ((0) - 0011, (1) - 1110, (2) - 0001)
 pen_state:    WORD 0x00   ; Перо поднято (0), опущено как перо (1), опущено как ластик (2)
-st_arr:       WORD 0xA85  ; Начало массива
-
+st_arr:       WORD 0x585  ; Начало массива
+              
   ; ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;; MAIN
-
-START: ;DI                      ; запрет прерываний
-       ;LD #0x9                 ; Разрешение прерываний и вектор 1
-       ;OUT 0x1                 ; Таймер на вектор INT_TIMER
-       ;LD blink_cursor_period  ; Таймер на 0.2 * 10 = 2 сек
-       ;OUT 0x0                 ; Запись в DR таймера
-       ;EI                      ; Прерываемся!!
-ind:   WORD 0x0000
-loop0: ;LD ind
-       ;INC
-                               ; OUT 0x10  ; Вывести правый байт в ВУ-6
-       ;ST ind
-       CALL tick
+  
+START: NOP
+       DI                      ; запрет прерываний
+       LD #0x9                 ; Разрешение прерываний и вектор 1
+       OUT 0x1                 ; Таймер на вектор INT_TIMER
+       LD blink_cursor_period  ; Таймер на 0.2 * 10 = 2 сек
+       OUT 0x0                 ; Запись в DR таймера
+       EI                      ; Прерываемся
+                               ; 
+loop0: DI
+       CALL tick               ; обработка клавиатуры
+       EI
+       NOP                     ; прерываемся только здесь, чтобы ничего не мешало выводу
 skip:  IN 0x7                  ; Читаем регистр состояния ВУ-3
        AND #0x40               ; Проверка на 6 бит
        BEQ loop0               ; Если не стоит, повторить
-       OUT 0x6                 ; Вывести правый байт в ВУ-3
-       HLT
-       CALL $show
+       IN 0x6                  ; Иначе стоп
        HLT
        JUMP loop0
-
+       
   ; ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;; FUNCTIONS
-
-tick:                   ; Обработка клавиатуры
-          IN 0x1D       ; Читаем регистр состояния ВУ-9
-          AND #0x40     ; Проверка на 6 бит
-          BEQ ret_0     ; Если не стоит, скип
+  
+tick:                 ; Обработка клавиатуры
+          IN 0x1D     ; Читаем регистр состояния ВУ-9
+          AND #0x40   ; Проверка на 6 бит
+          BEQ ret_0   ; Если не стоит, скип
           IN 0x1C
-          CMP #0x0D     ; режим курсора
+                      ; 
+          CMP #0x00   ; сброс курсора в 0
+          BZC skip_NLL
+          LD #0x00
+          ST curr_x
+          ST curr_y
+          LD #0x01
+          PUSH
+          PUSH
+          CALL move_cursor
+          JUMP ret_tick
+skip_NLL: CMP #0x0C   ; очистка холста
+          BZC skip_C
+          CALL $clear_arr
+          JUMP ret_tick
+skip_C:   CMP #0x0D   ; режим курсора
           BZC skip_00
           LD #0x00
           ST $pen_state
-          JUMP ret_tick
-skip_00:  CMP #0x0B     ; режим рисования
+          JUMP ret_0
+skip_00:  CMP #0x0B   ; режим рисования
           BZC skip_000
           LD #0x01
           ST $pen_state
-          JUMP ret_tick
-skip_000: CMP #0x0A     ; режим ластика
-          BZC skip_0    ; если нет - переходим к проверке на сдвиг курсора
+          JUMP ret_0
+skip_000: CMP #0x0A   ; режим ластика
+          BZC skip_0  ; если нет - переходим к проверке на сдвиг курсора
           LD #0x02
           ST $pen_state
-          JUMP ret_tick
-skip_0:   CMP #0x07     ; влево-вверх (x|, y-) (0 - в минус, 1 - в ноль, 2 - в плюс)
+          JUMP ret_0
+skip_0:   CMP #0x07   ; влево-вверх (x|, y-) (0 - в минус, 1 - в ноль, 2 - в плюс)
           BZC skip_1
           LD #0x02
           PUSH
           PUSH
           CALL move_cursor
           JUMP ret_tick
-skip_1:   CMP #0x08     ; вверх
+skip_1:   CMP #0x08   ; вверх
           BZC skip_2
           LD #0x02
           PUSH
@@ -82,7 +120,7 @@ skip_1:   CMP #0x08     ; вверх
           PUSH
           CALL move_cursor
           JUMP ret_tick
-skip_2:   CMP #0x09     ; вправо-вверх
+skip_2:   CMP #0x09   ; вправо-вверх
           BZC skip_3
           LD #0x02
           PUSH
@@ -90,7 +128,7 @@ skip_2:   CMP #0x09     ; вправо-вверх
           PUSH
           CALL move_cursor
           JUMP ret_tick
-skip_3:   CMP #0x04      ; влево
+skip_3:   CMP #0x04   ; влево
           BZC skip_4
           LD #0x01
           PUSH
@@ -98,7 +136,7 @@ skip_3:   CMP #0x04      ; влево
           PUSH
           CALL move_cursor
           JUMP ret_tick
-skip_4:   CMP #0x06     ; вправо
+skip_4:   CMP #0x06   ; вправо
           BZC skip_5
           LD #0x01
           PUSH
@@ -106,7 +144,7 @@ skip_4:   CMP #0x06     ; вправо
           PUSH
           CALL move_cursor
           JUMP ret_tick
-skip_5:   CMP #0x01     ; влево-вниз
+skip_5:   CMP #0x01   ; влево-вниз
           BZC skip_6
           LD #0x00
           PUSH
@@ -114,7 +152,7 @@ skip_5:   CMP #0x01     ; влево-вниз
           PUSH
           CALL move_cursor
           JUMP ret_tick
-skip_6:   CMP #0x02     ; вниз
+skip_6:   CMP #0x02   ; вниз
           BZC skip_7
           LD #0x00
           PUSH
@@ -122,34 +160,41 @@ skip_6:   CMP #0x02     ; вниз
           PUSH
           CALL move_cursor
           JUMP ret_tick
-skip_7:   CMP #0x01     ; вправо-вниз
+skip_7:   CMP #0x03   ; вправо-вниз
           BZC skip_8
           LD #0x00
           PUSH
           PUSH
           CALL move_cursor
           JUMP ret_tick
-skip_8:   NOP
-ret_tick: NOP;CALL $show
+skip_8:   JUMP ret_0
+ret_tick: CALL $show
 ret_0:    RET
-
-
+          
+          
+          
 move_cursor:             ; движение курсора на значения в стеке
              LD &1       ; смещение по y
              DEC         ; поправка для удобства
              ADD $curr_y
              CMP #0x00
              BLT skip_y  ; если получилось меньше 0 - скип
+             DEC
              CMP $max_y  ; если больше максимума - тоже скип
              BGE skip_y
+             INC
              ST $curr_y
 skip_y:      LD &2       ; смещение по x
              DEC         ; поправка для удобства
              ADD $curr_x
              CMP #0x00
              BLT skip_x  ; если получилось меньше 0 - скип
+             DEC
+             DEC
              CMP $max_x  ; если больше максимума - тоже скип
              BGE skip_x
+             INC
+             INC
              ST $curr_x
 skip_x:      POP
              SWAP
@@ -158,15 +203,21 @@ skip_x:      POP
              LD $curr_x
              OUT 0x06
              LD $curr_y
+             ASL
+             ASL
+             ASL
+             ASL
+             ADD $curr_x
              OUT 0x02
              CALL draw_dot
-             CALL show
              RET
-
+             
+             
+             
 mask:       WORD 0x00       ; будущая маска
 mask_cntr:  WORD 0x00       ; цикл
 y_pos:      WORD 0x00       ; для нахождение позиции y
-draw_dot:
+draw_dot:   
             LD $curr_x
             ST mask_cntr
             LD #0x01
@@ -178,20 +229,20 @@ loop_mask:  ASL
             LD $st_arr      ; готовим координату
             ADD $curr_y
             ST y_pos
-                            ;
+                            ; 
             LD $pen_state
             BZS skip_point  ; если режим курсора - скип
             DEC
             BZC eraser      ; если режим ластика => в AC 1
             LD #0x02
 eraser:     DEC             ; теперь 0й бит в AC - нужный цвет пикселя (x, y)
-                            ;
+                            ; 
             BZS draw_light  ; если рисуем, а не стираем
             LD (y_pos)
             OR mask
             ST (y_pos)
             RET
-draw_light:
+draw_light: 
             LD mask
             NEG
             ST mask
@@ -199,7 +250,9 @@ draw_light:
             AND mask
             ST (y_pos)
 skip_point: RET
-
+            
+            
+            
 cntr1:    WORD 0x00
 pointer1: WORD 0x00
 show:     NOP       ; обновление холста
@@ -212,10 +265,14 @@ show:     NOP       ; обновление холста
           CLA
 loop2:    LD -(pointer1)
           OUT 0x10  ; Пишем пустой столбец
-          LOOP cntr1
-          JUMP loop2
-          RET
-
+          NOP
+          
+  LOOP cntr1
+  JUMP loop2
+  RET
+  
+  
+  
 cntr:      WORD 0x00
 pointer:   WORD 0x00
 clear_arr: NOP           ; очистка холста
@@ -234,10 +291,10 @@ loop1:     CLA
            JUMP loop1
            CALL show
            RET
-
-
-                                          ; Возвращает текущее состояние точки курсора (0 или 1)
-get_cursor_state:
+           
+           
+           
+get_cursor_state:                         ; Возвращает текущее состояние точки курсора (0 или 1)
                    LD &1                  ; Состояние счётчика (случай счётчик == 0)
                    BZC if_cursor_state_1  ; Если не 0, дальше
                    LD $pen_state          ; Если 0, грузим состояние пера
@@ -282,25 +339,23 @@ if_cursor_state_3: LD $pen_state          ; Грузим состояние пе
                    RET
 if_1_3:            ST &1                  ; Возвращаем 0
                    RET
-
-
-INT_TIMER:
-                                    ; Инкрементируем счётчик состояния курсора
+                   
+                   
+                   
+INT_TIMER:                          ; Инкрементируем счётчик состояния курсора
            LD $cursor_state
            INC
            SUB #0x04
            BZS if1
            ADD #0x04
 if1:       ST $cursor_state
-                                    ; Получаем текущее сосотояние курсора
+                                    ; Получаем текущее состояние курсора
            PUSH
            CALL get_cursor_state
            POP
-           OUT 0x6
+           OUT 0x6                  ; вот здесь вот должна быть show, а в show отрисовка курсора, но бэвм медленная хрень
                                     ; Переставляем таймер
            LD $blink_cursor_period  ; Перезаводим эту хрень
            OUT 0x0                  ; Запись в DR таймера
            IRET
-
-
-  WORD 0x01, 0x02, 0x04, 0x02, 0x01
+           
